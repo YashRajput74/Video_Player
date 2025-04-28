529d194f2401f690f79839225a03fcd1bfb7a2ae
Yash Rajput <yashrajput7455@gmail.com>
2025-04-28 20:29:03 +0530

    added a button inside the controlBar

--- main.js
+++ main.js
@@ -9,6 +9,18 @@ document.addEventListener('DOMContentLoaded', () => {
         loop: true,
         muted: false,
     });
+    player.ready(()=>{
+        const controlBar=player.getChild('controlBar');
+        const customButton=player.controlBar.el().appendChild(document.createElement('div'));
+        customButton.className='vjs-control vjs-button custom-settings-button';
+        customButton.setAttribute("role","button");
+        customButton.setAttribute("tabindex","0");
+        customButton.setAttribute("aria-label","Custom Settings ");
+        customButton.innerHTML=`<span class="vjs-icon-placeholder">S</span>`;
+        customButton.addEventListener("click",()=>{
+            alert("clicked!")
+        })
+    })
     const player2=videojs('myVideo2',{
         autoplay: true,
         controls: true,
